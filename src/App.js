a07c59bc0e1b2cc9f77567fdd234c5477c0a6282
Jack <oportojack@gmail.com>
2022-06-03 23:02:18 -0400

    About page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/header/header";
+import About from "./components/about/about";
+import Menu from "./components/menu/menu";
+import Order from "./components/order/order";
 import Contact from "./components/contact/contact";
 import Footer from "./components/footer/footer";
 
@@ -8,8 +11,10 @@ function App() {
   return (
     <div>
       <Header />
-      <div className='px-5 font-tiro'>
-        <Contact />
+      <div className='px-5 font-tiro text-gray-600/80 '>
+        <About />
+        <Menu />
+        <Order />
         <Footer />
       </div>
     </div>
--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Contact = () => {
-  return <div className='border-2 text-center'>CONTACT US PLEASE</div>;
+  return (
+    <section>
+      <div className='container p-3 mx-auto lg:w-1/3 md:w-2/3'>
+        <div className=' mx-auto'>
+          <div className='rounded-lg  overflow-hidden align-top object-fit transition text-center'>
+            <h1>BIALYS - COFFEE - BREAKFAST & LUNCH </h1>
+            <p>TAKEAWAY ONLY </p>
+            <p> 7am-3pm</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
