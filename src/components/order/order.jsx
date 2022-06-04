fa04228546fa22a1023bb4b3429c6deead2318ab
Jack <oportojack@gmail.com>
2022-06-03 23:07:50 -0400

    Correct Order link

--- src/components/order/order.jsx
+++ src/components/order/order.jsx
@@ -7,10 +7,10 @@ const Order = () => {
       <div className='container py-8 mx-auto lg:w-3/5 '>
         <div className=' mx-auto'>
           <div className='rounded-lg overflow-hidden  text-center'>
-            <a href='https://img1.wsimg.com/isteam/ip/810cf8ff-cf4a-4605-ba00-c6974fb7e012/D77595AC-AEBA-49DE-996A-2B4197239FCA.jpeg/:/cr=t:20.85%25,l:0%25,w:100%25,h:50%25/rs=w:1240,h:620,cg:true'>
+            <a href='http://deli-desires.smartonlineorder.com/'>
               <img src={orderNow} alt='Order now!' />
             </a>
-            <p className='py-5 text-lg'>
+            <p className='py-5 md:text-lg'>
               For Pre-Orders & Online Ordering Please Click the image above :)
             </p>
           </div>
