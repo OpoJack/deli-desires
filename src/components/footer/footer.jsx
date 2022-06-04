aa3d562cf69776fff63808be58f0e1c3e475d20f
Jack <oportojack@gmail.com>
2022-06-04 01:31:33 -0400

    Adds Footer with link to github

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer className='border-2 text-center'>I AM THE FOOTER</footer>;
+  return (
+    <section>
+      <div className='container p-3 mx-auto lg:w-1/2 md:w-7/12 my-2 sm:w-10/12 '>
+        <div className=' mx-auto '>
+          <div className='rounded-lg overflow-hidden align-top transition text-center text-base'>
+            <a href='https://github.com/OpoJack' className='font-yellowtail'>
+              Page by Jack Oporto
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Footer;
