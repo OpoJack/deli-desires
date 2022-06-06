e2ddfe33e0687dbf3bcf3fa9d62c72c5df08e5f9
Jack <oportojack@gmail.com>
2022-06-05 22:28:22 -0400

    Restructure files, Includes route to /new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Original from "./pages/original";
+import New from "./pages/new";
 
 function App() {
   return (
     <BrowserRouter>
-      <Original />
+      <Routes>
+        <Route path='/' element={<Original />} />
+        <Route path='/new' element={<New />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/pages/original.jsx
+++ src/pages/original.jsx
@@ -1,11 +1,11 @@
 // import React from "react";
-import Header from "../components/header/header";
-import About from "../components/about/about";
-import Menu from "../components/menu/menu";
-import Order from "../components/order/order";
-import Contact from "../components/contact/contact";
-import SMedia from "../components/social-media/social-media";
-import Footer from "../components/footer/footer";
+import Header from "../components/original/header";
+import About from "../components/original/about";
+import Menu from "../components/original/menu";
+import Order from "../components/original/order";
+import Contact from "../components/original/contact";
+import SMedia from "../components/original/social-media";
+import Footer from "../components/original/footer";
 
 const Original = () => {
   return (
